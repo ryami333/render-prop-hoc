23250b7c7c51ce91e4c240d97b3318123c65b7a9
Mitch Ryan <mitch.ryan@outlook.co.nz>
2018-07-15 23:04:32 +1200

    Add tests

--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -1,5 +1,6 @@
 // @flow
 
+import { compose } from "recompose";
 import React from "react";
 import renderPropsHOC from "..";
 import { render } from "react-testing-library";
@@ -7,29 +8,77 @@ import { render } from "react-testing-library";
 describe("renderPropsHOC", () => {
 	const FOO: string & "FOO" = "FOO";
 	const BAR: string & "BAR" = "BAR";
+	const BAZ: string & "BAZ" = "BAZ";
 
 	type DemoProps = {
 		foo: string,
-		bar: string
+		bar: string,
+		baz: string
 	};
 
 	function FooConsumer({ children }: { children: string => React$Node }) {
 		return children(FOO);
 	}
 
-	// function BarConsumer({ children }: { children: string => React$Node }) {
-	// 	return children(BAR);
-	// }
-
-	function Demo({ foo, bar }: DemoProps) {
-		return `${foo},${bar}`;
+	function BarConsumer({ children }: { children: string => React$Node }) {
+		return children(BAR);
 	}
 
-	it("passes props to child", () => {
-		const mapProps = foo => ({ foo });
-		const ConnectedDemo = renderPropsHOC(FooConsumer, mapProps)(Demo);
-		const { queryByText } = render(<ConnectedDemo bar={BAR} />);
+	function Demo({ foo, bar, baz }: DemoProps) {
+		return `${foo},${bar},${baz}`;
+	}
 
-		expect(queryByText(`${FOO},${BAR}`)).toBeTruthy();
+	const mapFooProps = foo => ({ foo });
+	const mapBarProps = bar => ({ bar });
+
+	describe("single", () => {
+		const ConnectedDemo = renderPropsHOC(FooConsumer, mapFooProps)(Demo);
+
+		it("passes props to child", () => {
+			const { queryByText } = render(
+				<ConnectedDemo bar={BAR} baz={BAZ} />
+			);
+
+			expect(queryByText(`${FOO},${BAR},${BAZ}`)).toBeTruthy();
+		});
+
+		/* eslint-disable no-unused-expressions */
+
+		it("doesnt throw an error if render prop not given", () => {
+			<ConnectedDemo bar={BAR} baz={BAZ} />;
+		});
+
+		it("throws an error if remaining props are not given", () => {
+			// $ExpectError
+			<ConnectedDemo bar={BAR} />;
+			// $ExpectError
+			<ConnectedDemo baz={BAZ} />;
+		});
+	});
+
+	describe("stacked", () => {
+		const ConnectedDemo = compose(
+			renderPropsHOC(BarConsumer, mapBarProps),
+			renderPropsHOC(FooConsumer, mapFooProps)
+		)(Demo);
+
+		it("passes both props to child", () => {
+			const { queryByText } = render(<ConnectedDemo baz={BAZ} />);
+
+			expect(queryByText(`${FOO},${BAR},${BAZ}`)).toBeTruthy();
+		});
+
+		/* eslint-disable no-unused-expressions */
+
+		it("doesnt throw an error if render props not given", () => {
+			<ConnectedDemo baz={BAZ} />;
+		});
+
+		it("throws an error if remaining props are not given", () => {
+			// $ExpectError
+			<ConnectedDemo bar={BAR} />;
+			// $ExpectError
+			<ConnectedDemo baz={BAZ} />;
+		});
 	});
 });
